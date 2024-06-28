125d329742752b39f0f6c88bd8db137711fff3cd
Hazem <hazem.ismail@hotmail.com>
2024-06-28 20:09:24 +0300

    convert to setup store

--- src/stores/TaskStore.js
+++ src/stores/TaskStore.js
@@ -11,65 +11,68 @@ import {
 } from "firebase/firestore";
 
 import { defineStore } from "pinia";
+import { computed, ref } from "vue";
 
 const tasksCollectionRef = collection(db, "tasks");
 const tasksCollectionQuery = query(tasksCollectionRef, orderBy("date", "desc"));
 
-export const useTaskStore = defineStore("taskStore", {
-  state: () => ({
-    tasks: [],
-    isLoading: false,
-  }),
-  getters: {
-    favs() {
-      return this.tasks.filter((t) => t.isFav);
-    },
-    favCount() {
-      return this.tasks.reduce((accumlator, currentValue) => {
+export const useTaskStore = defineStore("taskStore", () => {
+  const tasks = ref([]),
+    isLoading = ref(false),
+    favs = computed(() => tasks.value.filter((t) => t.isFav)),
+    totalCount = computed(() => tasks.value.length),
+    favCount = computed(() => {
+      return tasks.value.reduce((accumlator, currentValue) => {
         return currentValue.isFav ? accumlator + 1 : accumlator;
       }, 0);
-    },
-    totalCount: (state) => {
-      return state.tasks.length;
-    },
-  },
-  actions: {
-    getTasks() {
-      this.isLoading = true;
-      onSnapshot(tasksCollectionQuery, (querySnapshot) => {
-        const fbTasks = [];
-        querySnapshot.forEach((doc) => {
-          const task = {
-            id: doc.id,
-            title: doc.data().title,
-            isFav: doc.data().isFav,
-          };
-          fbTasks.push(task);
-        });
-        this.tasks = fbTasks;
-      });
-      this.isLoading = false;
-    },
+    });
 
-    addTask(task) {
-      try {
-        addDoc(tasksCollectionRef, {
-          title: task.title,
-          isFav: task.isFav,
-          date: Date.now(),
-        });
-      } catch (e) {
-        console.error("Error adding document: ", e);
-      }
-    },
-    deleteTask(id) {
-      deleteDoc(doc(tasksCollectionRef, id));
-    },
-    toggleFav(id) {
-      const task = this.tasks.find((t) => t.id === id);
-      updateDoc(doc(tasksCollectionRef, id), {
-        isFav: !task.isFav,
+  function getTasks() {
+    isLoading.value = true;
+    onSnapshot(tasksCollectionQuery, (querySnapshot) => {
+      const fbTasks = [];
+      querySnapshot.forEach((doc) => {
+        const task = {
+          id: doc.id,
+          title: doc.data().title,
+          isFav: doc.data().isFav,
+        };
+        fbTasks.push(task);
       });
-    },
-  },
+      tasks.value = fbTasks;
+    });
+    isLoading.value = false;
+  }
+
+  function addTask(task) {
+    try {
+      addDoc(tasksCollectionRef, {
+        title: task.title,
+        isFav: task.isFav,
+        date: Date.now(),
+      });
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  }
+  function deleteTask(id) {
+    deleteDoc(doc(tasksCollectionRef, id));
+  }
+  function toggleFav(id) {
+    const task = tasks.value.find((t) => t.id === id);
+    updateDoc(doc(tasksCollectionRef, id), {
+      isFav: !task.isFav,
+    });
+  }
+  return {
+    tasks,
+    isLoading,
+    favs,
+    favCount,
+    totalCount,
+    getTasks,
+    addTask,
+    deleteTask,
+    toggleFav,
+  };
 });
